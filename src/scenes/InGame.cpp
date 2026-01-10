f74c35fb0f20a9e2af8db42a28a55961a892747b
JulesDELTEIL <jules.delteil@epitech.eu>
2026-01-10 15:42:46 +0100

    feat(champions): movements change animations frames

--- src/scenes/InGame.cpp
+++ src/scenes/InGame.cpp
@@ -13,7 +13,6 @@
 #include <physic/components/velocity.hpp>
 #include <physic/components/position.hpp>
 #include <interaction/components/player.hpp>
-#include <sfml/components/sprite.hpp>
 
 #include "scenes.hpp"
 #include "components/stat_pool.hpp"
@@ -25,20 +24,20 @@ void setInGameScene(Game& game) {
         {"poll_event"},  // INPUT
         {},  // PRE UPDATE
         {"animate", "movement2"},  // UPDATE
-        {"follow_player"},  // POST UPDATE
+        {"follow_player", "champion_movement"},  // POST UPDATE
         {"draw", "display"}  // RENDER
     }};
 
     ingame.entities = {
         {game.nextEntity(MAP), "sumoners_rift"},
-        {game.nextEntity(PLAYER), "ethan", {150, 7100}}
+        {game.nextEntity(PLAYER), "ethan"}
     };
 
     std::size_t idx = game.addScene(ingame);
     game.subForScene<te::Keys>(idx, "key_input", [&game](te::Keys keys) {
     });
-    game.subForScene<te::Mouse>(idx, "mouse_input", [&game](te::Mouse ev) {
-        if (ev.type[te::MouseEvent::MouseRight]) {
+    game.subForScene<te::Mouse>(idx, "mouse_input", [&game](te::Mouse mouse) {
+        if (mouse.type[te::MouseEvent::MouseRight]) {
             static te::Timestamp delta(0.02f);
             if (!delta.checkDelay())
                 return;
@@ -51,8 +50,8 @@ void setInGameScene(Game& game) {
                 ECS::DenseZipper(players, vels, posis, stats)) {
                     mat::Vector2f actual_click;
                     //  MAGIC VALUE, TO CHANGE WHEN WINDOW SIZE CHANGE
-                    actual_click.x = pos.x - (1280.f / 2.f) + ev.position.x;
-                    actual_click.y = pos.x - (720.f / 2.f) + ev.position.y;
+                    actual_click.x = pos.x - (1280.f / 2.f) + mouse.position.x;
+                    actual_click.y = pos.y - (720.f / 2.f) + mouse.position.y;
                     mat::Vector2f dir(actual_click - pos);
                     float length = std::sqrt(dir.x * dir.x + dir.y * dir.y);
                     dir /= length;
