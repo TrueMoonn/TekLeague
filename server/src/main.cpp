fa59452ab3d9a4815f37f63986a5b4f3b7640592
PierrePruvost03 <pierre.pruvost@epitech.eu>
2026-01-12 20:05:57 +0100

    feat(server): basic setup for server

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -8,8 +8,18 @@
 #include "Server.hpp"
 #include "scenes.hpp"
 
-int main() {
-    Server game;
+int main(int ac, char **av) {
+    uint16_t port = 8080;
+    std::string protocol = "UDP";
+    size_t max_clients = 4;
+
+    if (ac > 1) {
+        port = static_cast<uint16_t>(std::stoi(av[1]));
+    }
+    if (ac > 2) {
+        protocol = av[2];
+    }
+    Server game(port, protocol);
     for (std::size_t i = 0; i < SCAST(SCENES::LIMITSCENE); ++i)
         SCENES_SETUPS.at(static_cast<SCENES>(i))(game);
 
