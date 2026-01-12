0a0c93934ff143f0f1899a8e752e039824099d71
PierrePruvost03 <pierre.pruvost@epitech.eu>
2026-01-12 20:51:11 +0100

    fix(server): bring back port to uin16. It will never go this far

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -9,11 +9,11 @@
 #include "scenes.hpp"
 
 int main(int ac, char **av) {
-    uint32_t port = 6767;
+    uint16_t port = 6767;
     std::string protocol = "UDP";
 
     if (ac > 1) {
-        port = static_cast<uint32_t>(std::stoi(av[1]));
+        port = static_cast<uint16_t>(std::stoi(av[1]));
     }
     if (ac > 2) {
         protocol = av[2];
