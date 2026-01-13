a388b58ad019bcef26348b478fdd90c4375d287d
PierrePruvost03 <pierre.pruvost@epitech.eu>
2026-01-13 15:29:31 +0100

    feat(server): implementation exemple on how to set automatic senders + default config (will be updated soon)

--- server/include/Server.hpp
+++ server/include/Server.hpp
@@ -7,22 +7,49 @@
 
 #pragma once
 
+    #include <cstdint>
     #include <string>
     #include <vector>
 
     #include <network/GameServer.hpp>
     #include <Network/ProtocolManager.hpp>
     #include "Game.hpp"
+    #include "clock.hpp"
 
 class Server : public te::network::GameServer, public Game {
+ public:
+    ////// Timestamp default latency in seconds //////
+    static constexpr float PLAYERS_UPDATES_DEFAULT_LATENCY = 1.0f / 60.0f;
+
  public:
     explicit Server(uint16_t port = 6767, const std::string& protocol = "UDP");
 
     void run() override;
 
+    void sendAutomatic();
+
+    /**
+     * @brief Set how often to send PLAYERS_UPDATES (in seconds)
+     */
+    void setPlayersUpdateFrequency(float frequency) {
+        players_update_interval = frequency;
+        players_update_timestamp.delay = size_t(SEC_TO_MICRO(frequency));
+    }
+
  private:
     bool setPacketsHandlers();
 
+    std::uint32_t next_player_id;
+
+    ////// Timestamp //////
+    te::Timestamp players_update_timestamp = te::Timestamp(PLAYERS_UPDATES_DEFAULT_LATENCY);
+    float players_update_interval = PLAYERS_UPDATES_DEFAULT_LATENCY;
+
+    ////// Handlers //////
     void handleConnectionRequest(const std::vector<uint8_t>& data,
         const net::Address& sender);
+
+    ////// Senders //////
+    void sendPlayersUpdate();
+
 };
--- server/src/Server.cpp
+++ server/src/Server.cpp
@@ -5,16 +5,31 @@
 ** Server.cpp
 */
 
+#include <cstdint>
 #include <string>
 #include <vector>
 
+#include <ECS/DenseSA.hpp>
+#include <ECS/DenseZipper.hpp>
+
+#include <Network/generated_messages.hpp>
+
+#include <physic/components/position.hpp>
+#include <entity_spec/components/health.hpp>
+#include <entity_spec/components/team.hpp>
+#include <interaction/components/player.hpp>
+
 #include <network/GameServer.hpp>
+
 #include "Server.hpp"
+#include "ECS/Registry.hpp"
 
 Server::Server(uint16_t port, const std::string& protocol) :
-    te::network::GameServer(port, protocol),
-    Game("server/plugins") {
+    te::network::GameServer(port, protocol)
+    , Game("server/plugins")
+    , next_player_id(2) {
     start();
+    startNetworkThread();
     setPacketsHandlers();
 }
 
@@ -31,9 +46,45 @@ void Server::handleConnectionRequest(const std::vector<uint8_t>& data,
 
 }
 
+void Server::sendAutomatic() {
+    if (players_update_timestamp.checkDelay()) {
+        sendPlayersUpdate();
+    }
+}
+
+void Server::sendPlayersUpdate() {
+    ::net::PLAYERS_UPDATES msg;
+    ECS::Registry registry = getRegistry();
+
+    auto& positions = registry.getComponents<addon::physic::Position2>();
+    auto& healths = registry.getComponents<addon::eSpec::Health>();
+    auto& players = registry.getComponents<addon::intact::Player>();
+
+    for (auto&& [player, pos, health] :
+        ECS::DenseZipper(players, positions, healths)) {
+        ::net::PlayerUpdate state;
+        state.x = pos.x;
+        state.y = pos.y;
+        state.hp = static_cast<double>(health.amount);
+        state.id = 0;  // TODO(xxx): à voir comment on gère leur id
+        state.level = 0.0;  // TODO(xxx): implement later
+        state.mana = 0.0;    // TODO(xxx): implement later
+        state.direction = 0;  // TODO(xxx): implement later
+        std::memset(state.effects, 0, sizeof(state.effects));  // TODO(xxx): implement later
+
+        msg.players.push_back(state);
+    }
+
+    if (!msg.players.empty()) {
+        broadcastToAll(msg.serialize());
+    }
+}
+
 void Server::run() {
     while (isRunning()) {
         if (_framelimit.checkDelay())
             runSystems();
+        update(0);
+        sendAutomatic();
     }
 }
