fca33294a6b4bda8cf430269d24bc6548c01a23f
JulesDELTEIL <jules.delteil@epitech.eu>
2026-01-13 15:38:21 +0100

    fix(inventory): creation with toml

--- src/components/inventory.cpp
+++ src/components/inventory.cpp
@@ -12,11 +12,12 @@
 void registerInventory(Game& game) {
     game.registerComponent<Inventory>("inventory",
         [&game](ECS::Entity e, const toml::table& params) {
-        std::array<std::size_t, MAX_ITEM_SLOT> arr(0);
+        std::array<std::size_t, MAX_ITEM_SLOT> arr;
+        arr.fill(0);
         if (params["slots"].is_array()) {
             const auto& slots = params["slots"].as_array();
             for (std::size_t i = 0; i < MAX_ITEM_SLOT; ++i) {
-                arr[i] = slots.at(i).value_or(0);
+                arr[i] = slots->at(i).value_or(0);
             }
         }
         game.createComponent<Inventory>(e, arr);
