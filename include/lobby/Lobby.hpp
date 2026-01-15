aa8af81544dfbf089d9bae277c275f8b24675fdb
baptiste <baptiste.dunes@epitech.eu>
2026-01-15 01:45:28 +0100

    feat(autosend): server sending game data automatically

--- include/lobby/Lobby.hpp
+++ include/lobby/Lobby.hpp
@@ -8,14 +8,26 @@
 #pragma once
 
     #include <string>
+    #include <chrono>
+    #include <vector>
+
+    #include <Network/generated_messages.hpp>
 
     #include "Game.hpp"
+
 
 class Lobby : public Game {
  public:
     static constexpr std::string PLUGINS_PATH = "server/plugins";
  public:
     Lobby(uint max_players, const std::string& code);
+
+    std::chrono::_V2::system_clock::time_point game_start_time =
+        std::chrono::system_clock::now();
+
+    net::TeamScore teams[2];
+
+    std::vector<net::ScoreboardEntry> scoreboard;
 
     void run() override;
 
