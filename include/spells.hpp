82cf7deafa12bc4480e18c1b93de3ea3d251c796
PierrePruvost03 <pierre.pruvost@epitech.eu>
2026-01-18 02:49:56 +0100

    feat(spells): goatier second spells (a dash)

--- include/spells.hpp
+++ include/spells.hpp
@@ -19,12 +19,14 @@ enum class SpellId {
     GULES_1,
     GULES_2,
     GOATIER_1,
+    GOATIER_2,
 };
 
 void noSpell(Game&, ECS::Entity, const mat::Vector2f&);
 void gulesSpell1(Game&, ECS::Entity, const mat::Vector2f&);
 void gulesSpell2(Game&, ECS::Entity, const mat::Vector2f&);
 void goatierSpell1(Game&, ECS::Entity, const mat::Vector2f&);
+void goatierSpell2(Game&, ECS::Entity, const mat::Vector2f&);
 
 static const std::unordered_map<SpellId,
     std::function<void(Game&, ECS::Entity, const mat::Vector2f&)>> SPELLS = {
@@ -32,4 +34,5 @@ static const std::unordered_map<SpellId,
     {SpellId::GULES_1, &gulesSpell1},
     {SpellId::GULES_2, &gulesSpell2},
     {SpellId::GOATIER_1, &goatierSpell1},
+    {SpellId::GOATIER_2, &goatierSpell2},
 };
